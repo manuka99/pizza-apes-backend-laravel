330e86b6ac6b0b478511dd17832c32d0f28fba2c
manuka <manukayasas99@gmail.com>
2021-02-03 01:24:56 +0530

    session data pretty time

--- app/Models/SessionData.php
+++ app/Models/SessionData.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,14 @@ class SessionData extends Model
     protected $table = "sessions";
     protected $keyType = 'string';
     public $timestamps = false;
+
+    protected $appends = ['last_online'];
+
+    public function getLastOnlineAttribute()
+    {
+        $moment = Helper::preetyTime($this->last_activity);
+        if ($moment === "Just now")
+            return "Online";
+        return $moment;
+    }
 }
